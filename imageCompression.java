8e86f7efd2612bbdd57de8046818598bd270c1d2
Kartik Arora <kartikar@uw.edu>
2018-04-07 23:16:42 -0700

    Add compress method

--- imageCompression.java
+++ imageCompression.java
@@ -11,32 +11,29 @@ public class imageCompression {
 
     BufferedImage image = ImageIO.read(imageCompression.class.getResource("test.jpg"));
     PrintStream output = new PrintStream(new File("test2.txt"));
-
+    File inputFile = new File("test2.txt");
+    Scanner sc = new Scanner(inputFile);
     System.out.println("Testing convertTo2DUsingGetRGB:");
-
     int[][] result = convertTo2DUsingGetRGB(image, output);
     int width = image.getWidth();
     int height = image.getHeight();
     BufferedImage image2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    File inputFile = new File("test2.txt");
-    Scanner sc = new Scanner(inputFile);
-
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
         Color rgb = new Color(sc.nextInt(), sc.nextInt(), sc.nextInt());
         image2.setRGB(x, y, rgb.getRGB());
       }
     }
-
     File outputFile = new File("output.jpg");
     ImageIO.write(image2, "jpg", outputFile);
+    PrintStream outputCompressed = new PrintStream(new File("compressed.txt"));
+    compress(inputFile, outputCompressed);
   }
 
   private static int[][] convertTo2DUsingGetRGB(BufferedImage image, PrintStream output) {
     int width = image.getWidth();
     int height = image.getHeight();
     int[][] result = new int[height][width];
-
     for (int row = 0; row < height; row++) {
       for (int col = 0; col < width; col++) {
         result[row][col] = image.getRGB(col, row);
@@ -49,7 +46,25 @@ public class imageCompression {
       }
       output.println();
     }
-
     return result;
+  }
+
+  public static void compress(File inputFile, PrintStream outputCompressed)
+      throws FileNotFoundException {
+    Scanner outerSc = new Scanner(inputFile);
+    while (outerSc.hasNextLine()) {
+      Scanner innerSc = new Scanner(outerSc.nextLine());
+      outputCompressed.print(innerSc.nextInt() + " ");
+      while (innerSc.hasNextInt()) {
+        int val = 0;
+        if (innerSc.hasNextInt()) {
+          val = innerSc.nextInt();
+        } else {
+          break;
+        }
+        outputCompressed.print(val + " ");
+      }
+      outputCompressed.println();
+    }
   }
 }
